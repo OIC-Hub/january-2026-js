7427b88d302fa8a3f26e84befa19c9277da672d4
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-01-12 11:27:46 +0100

    added intro class files

---
+++ WK1-1-intro.js
@@ -0,0 +1,4 @@
+document.write("This is written using external JavaScript.");
+
+document.getElementById("place").innerText = "This place is Osogbo"
+document.getElementById("place2").innerHTML = "<b>This place is Akure</b>"
